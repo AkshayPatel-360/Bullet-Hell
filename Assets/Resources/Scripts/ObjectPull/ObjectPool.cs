c0d6a4709ae003f79b9a85814c3d049f2809c180
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2020-12-04 17:32:18 -0500

    Shoot Complet

---
+++ Assets/Resources/Scripts/ObjectPull/ObjectPool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static ObjectPool instance;
+
+    public static ObjectPool Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ObjectPool();
+            }
+            return instance;
+        }
+    }
+
+    Transform objectPoolParent;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject bullet;
+
+    public float bulletSpeed = 2;
+    private float timeBetweenShoot = 0.2f ;
+    
+
+
+
+    void Start()
+    {
+        bullet = Resources.Load<GameObject>("Prefabs/EggBullet");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+    }
+
+
+
+    void shoot()
+    {
+        if (Input.GetKey(KeyCode.Space))
+
+        {
+           // Vector2 randV2 = new Vector2 (Random.Range(-360, 360), Random.Range(-360, 360));
+
+            Vector2 randV2 = Random.insideUnitCircle.normalized * 5;
+            if (timeBetweenShoot <=0)
+            {
+
+                GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+
+                newBullet.GetComponent<Rigidbody2D>().velocity =randV2.normalized*bulletSpeed * Time.deltaTime;
+
+
+                timeBetweenShoot = 0.2f;
+
+            }
+           
+            timeBetweenShoot -= Time.deltaTime;
+
+        }
+    }
+
+
+    
+}
